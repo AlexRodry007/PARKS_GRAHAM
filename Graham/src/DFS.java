e0034f6e4dc8109b84dc8f22b7e6f3b3b8f8e145
AlexRodry007 <55761576+AlexRodry007@users.noreply.github.com>
2023-04-30 18:09:35 +0300

    8

--- Graham/src/DFS.java
+++ Graham/src/DFS.java
@@ -5,7 +5,7 @@ import parcs.*;
 public class DFS implements AM {
     public void run(AMInfo info) {
         Point[] n = (Point[])info.parent.readObject();
-        Point[] convex = ConvexHull.convexHull(n);
+        Point[] convex = ConvexHull.convexHull(n, n.length);
         info.parent.write(convex);
     }
 }
