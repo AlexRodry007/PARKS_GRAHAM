e9806c625ddcb6392024d5a89912be30b9f9fde2
AlexRodry007 <55761576+AlexRodry007@users.noreply.github.com>
2023-04-30 17:15:45 +0300

    7

--- Graham/src/Point.java
+++ Graham/src/Point.java
@@ -3,17 +3,18 @@ import java.util.ArrayList;
 import java.io.Serializable;
 import java.util.*;
 
-public class Point 
-{
-    int x, y;
-    Point()
-    {
-        x = 0;
-        y = 0;
-    }
-    Point(int a, int b)
-    {
-        x = a;
-        y = b;
-    }
+public class Point implements Serializable {
+private static final long serialVersionUID = 1L;
+
+int x, y;
+
+Point() {
+    x = 0;
+    y = 0;
+}
+
+Point(int a, int b) {
+    x = a;
+    y = b;
+}
 }
